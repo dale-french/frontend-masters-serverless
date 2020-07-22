842afc8ba63051f3e036212c2f2faf544dcd6e1f
Dale French <dalef@fmi.co.za>
2020-07-22 18:48:18 +0200

    Get scores from omdbapi

--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,32 @@
+const { URL } = require("url");
+const fetch = require("node-fetch");
 const movies = require("../data/movies.json");
 
 exports.handler = async () => {
+  const api = new URL("https://www.omdbapi.com/");
+
+  api.searchParams.set("apikey", process.env.OMDB_API_KEY);
+
+  const promises = movies.map((movie) => {
+    // use the moivies imdp id to lookup details
+    api.searchParams.set("i", movie.id);
+
+    return fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        const scores = data.Ratings;
+
+        return {
+          ...movie,
+          scores,
+        };
+      });
+  });
+
+  const moviesWithRatings = await Promise.all(promises);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(movies), // must have a string body
+    body: JSON.stringify(moviesWithRatings), // must have a string body
   };
 };
